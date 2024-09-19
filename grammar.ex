d668c501b088518df27cff6d04a298825fc38aa5
Nicolas Michel <nicolas.michel.lava@gmail.com>
2024-09-19 15:44:10 +0200

    Compute firsts per clause

--- grammar.ex
+++ grammar.ex
@@ -5,6 +5,14 @@ defmodule Tokenizer do
     struct(__MODULE__, tokens: String.split(input, ~r/\s+/, trim: true))
   end
 
+  def current_token(%__MODULE__{tokens: [token | tail]} = tokenizer) do
+    {token, tokenizer}
+  end
+
+  def current_token(%__MODULE__{tokens: []} = tokenizer) do
+    {nil, tokenizer}
+  end
+
   def next_token(%__MODULE__{tokens: [token | tail]} = tokenizer) do
     {token, struct(tokenizer, tokens: tail)}
   end
@@ -23,7 +31,7 @@ defmodule Grammar do
       struct(__MODULE__, def: def)
     end
 
-    def set_first(%__MODULE__{firsts: nil} = clause, firsts) when is_list(firsts) do
+    def set_firsts(%__MODULE__{firsts: nil} = clause, firsts) when is_list(firsts) do
       %{clause | firsts: firsts}
     end
   end
@@ -32,16 +40,28 @@ defmodule Grammar do
     @enforce_keys [:name, :clauses]
     defstruct [:name, :clauses]
 
-    def new(name, def) when is_atom(name) and is_list(def)do
+    def new(name) when is_atom(name) do
+      struct(__MODULE__, name: name, clauses: [])
+    end
+
+    def new(name, def) when is_atom(name) and is_list(def) do
       struct(__MODULE__, name: name, clauses: [Clause.new(def)])
     end
 
     def add_clause(%__MODULE__{clauses: clauses} = rule, def) when is_list(def) do
       %{rule | clauses: clauses ++ [Clause.new(def)]}
     end
+
+    def add_clause(%__MODULE__{clauses: clauses} = rule, %Clause{} = clause) do
+      %{rule | clauses: clauses ++ [clause]}
+    end
+
+    def get_firsts(%__MODULE__{clauses: clauses}) do
+      Enum.flat_map(clauses, & &1.firsts)
+    end
   end
 
-  def check_production_chain(rules) when is_map(rules) do
+  def check_all_rules_exist(rules) when is_map(rules) do
     Enum.map(rules, fn {name, %Rule{name: name, clauses: clauses}} ->
       Enum.map(clauses, fn %Clause{def: def} ->
         Enum.map(def, fn
@@ -54,6 +74,27 @@ defmodule Grammar do
     |> List.flatten()
   end
 
+  def check_rules_are_not_ambiguous(rules) do
+    Enum.flat_map(rules, fn {rule_name, %Rule{name: rule_name, clauses: clauses}} ->
+      clauses
+      # Add positionnal index to clauses for easier identification
+      |> Enum.with_index()
+      # Map each first to its clause index
+      |> Enum.flat_map(fn {%Clause{firsts: firsts}, id} ->
+        Enum.map(firsts, & {&1, id})
+      end)
+      # Group by first
+      |> Enum.group_by(& elem(&1, 0), & elem(&1, 1))
+      # Remove duplicate ids (in case of ambiguous first, it propagates upstream)
+      |> Enum.map(fn {first, clause_ids} -> {first, Enum.uniq(clause_ids)} end)
+      # Keep only ambiguous firsts (i.e. appearing in more than one clause)
+      |> Enum.filter(fn {_first, clause_ids} -> Enum.count(clause_ids) > 1 end)
+      |> Enum.map(& Tuple.insert_at(&1, 0, rule_name))
+    end)
+    # filter out rules without conflicts
+    |> Enum.filter(fn {rule_name, _first, clause_ids} -> Enum.count(clause_ids) > 0 end)
+  end
+
   def first_of_rules(rules) when is_map(rules) do
     Enum.reduce(rules, %{}, &firsts_of_rule(&1, &2, rules))
   end
@@ -62,23 +103,25 @@ defmodule Grammar do
     acc
   end
 
-  def firsts_of_rule({name, %Rule{name: name, clauses: clauses}}, acc, rules) do
+  def firsts_of_rule({name, %Rule{name: name, clauses: clauses}} = rule, acc, rules) do
+    acc = Map.put(acc, name, Rule.new(name))
     Enum.reduce(clauses, acc, &firsts_of_clause(name, &1, &2, rules))
+
+    # TODO Check firsts do not overlap
+    # if current_rule_firsts && Enum.any?(sub_rule_firsts, & &1 in current_rule_firsts) do
+    #   raise "Firsts from #{sub_rule_name} already found for rule #{rule_name}"
+    # end
   end
 
-  def firsts_of_clause(rule_name, %Clause{def: [sub_rule_name | _]}, acc, _rules) when is_atom(sub_rule_name) and is_map_key(acc, sub_rule_name) do
+  def firsts_of_clause(rule_name, %Clause{def: [sub_rule_name | _], firsts: nil} = clause, acc, _rules) when is_atom(sub_rule_name) and is_map_key(acc, sub_rule_name) do
     # sub_rule already processed
 
-    sub_rule_firsts = Map.get(acc, sub_rule_name)
-    current_rule_firsts = Map.get(acc, rule_name)
-    if current_rule_firsts && Enum.any?(sub_rule_firsts, & &1 in current_rule_firsts) do
-      raise "Firsts from #{sub_rule_name} already found for rule #{rule_name}"
-    end
-
-    Map.update(acc, rule_name, sub_rule_firsts, & &1 ++ sub_rule_firsts)
+    sub_rule_firsts = acc |> Map.get(sub_rule_name) |> Rule.get_firsts()
+    updated_clause = Clause.set_firsts(clause, sub_rule_firsts)
+    Map.update!(acc, rule_name, & Rule.add_clause(&1, updated_clause))
   end
 
-  def firsts_of_clause(rule_name, %Clause{def: [sub_rule_name | _]} = clause, acc, rules) when is_atom(sub_rule_name) do
+  def firsts_of_clause(rule_name, %Clause{def: [sub_rule_name | _], firsts: nil} = clause, acc, rules) when is_atom(sub_rule_name) do
     # sub_rule *not* already processed
 
     sub_rule = Map.get(rules, sub_rule_name)
@@ -86,14 +129,19 @@ defmodule Grammar do
     firsts_of_clause(rule_name, clause, acc, rules)
   end
 
-  def firsts_of_clause(name, %Clause{def: [first | _]}, acc, _rules), do: Map.update(acc, name, [first], & &1 ++ [first])
+  def firsts_of_clause(rule_name, %Clause{def: [first | _]} = clause, acc, _rules) do
+    updated_clause= Clause.set_firsts(clause, [first])
+    Map.update(acc, rule_name, Rule.new(rule_name, [first]), & Rule.add_clause(&1, updated_clause))
+  end
 
   # def build_matcher_for(c) when is_binary(c), do: & &1 === c
   # def build_matcher_for(%Regex{} = r), do: & Regex.match?(r, &1)
 
   defmacro rule({name, _meta, def}, do: _blk) when is_atom(name) do
     quote do
-      rules = Map.update(@rules, unquote(name), Rule.new(unquote(name), unquote(def)), & Rule.add_clause(&1, unquote(def)))
+      rules = Module.get_attribute(unquote(__CALLER__.module), :rules)
+      if rules == %{}, do: Module.put_attribute(unquote(__CALLER__.module), :start_rule_name, unquote(name))
+      rules = Map.update(rules, unquote(name), Rule.new(unquote(name), unquote(def)), & Rule.add_clause(&1, unquote(def)))
       Module.put_attribute(unquote(__CALLER__.module), :rules, rules)
     end
   end
@@ -111,9 +159,9 @@ defmodule Grammar do
   defmacro __before_compile__(_env) do
     all_rules = Module.get_attribute(__CALLER__.module, :rules)
 
-    # Check for missing rules definition
+    # Check for missing rule declarations
     #
-    case Grammar.check_production_chain(all_rules) do
+    case Grammar.check_all_rules_exist(all_rules) do
       [] -> :ok
       errors ->
         errors = Enum.map_join(errors, "\n", fn {name, step} -> "| #{name} misses #{step}" end)
@@ -122,29 +170,45 @@ defmodule Grammar do
 
     # Build firsts map
     #
-    firsts_for_rules = first_of_rules(all_rules)
+    rules_with_firsts = first_of_rules(all_rules)
 
-    # productions =
-    #   for {name, defs} <- Enum.group_by(all_rules, & &1.name, & &1.def) do
-    #     quote do
-    #       def unquote(name)(%Tokenizer{} = tokenizer) do
-    #         IO.inspect(unquote(name), label: "RULE")
-    #         IO.inspect(tokenizer, label: "TOKENIZER")
-    #       end
-    #     end
-    #   end
-
-    # [start | _] = all_rules
-
-    quote do
-      def parse(input) do
-        tokenizer = Tokenizer.new(input)
-        # unquote(start.name)(tokenizer)
-      end
+    # Check for ambiguous rules
+    case Grammar.check_rules_are_not_ambiguous(rules_with_firsts) do
+      [] -> :ok
+      errors ->
+        errors = Enum.map_join(errors, "\n", fn {name, first, clause_ids} ->
+          "| Rule #{name} has conflicts for first #{inspect(first)} on clauses #{inspect(clause_ids)}"
+        end)
+        raise "\n* Ambiguous rules\n#{errors}"
     end
 
-    #   unquote_splicing(productions)
-    # end
+    IO.inspect(rules_with_firsts, label: "FIRSTS")
+
+    productions =
+      for {name, %Rule{name: name, clauses: clauses}} <- rules_with_firsts do
+        quote do
+          def unquote(name)(%Tokenizer{} = tokenizer) do
+            IO.inspect(unquote(name), label: "RULE")
+            IO.inspect(tokenizer, label: "TOKENIZER")
+          end
+        end
+      end
+
+    start_rule_name = Module.get_attribute(__CALLER__.module, :start_rule_name)
+    start = Map.get(rules_with_firsts, start_rule_name)
+
+    quote do
+      def rules do
+        unquote(Macro.escape(rules_with_firsts))
+      end
+
+      def parse(input) do
+        tokenizer = Tokenizer.new(input)
+        unquote(start.name)(tokenizer)
+      end
+
+      unquote_splicing(productions)
+    end
   end
 end
 
