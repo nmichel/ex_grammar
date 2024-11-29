86cd560a2e2634d3d8338198fa74e610da4b7cf9
Nicolas Michel <nicolas.michel.lava@gmail.com>
2024-11-29 15:42:08 +0100

    Setup CI

--- mix.exs
+++ mix.exs
@@ -30,7 +30,8 @@ defmodule Project do
     [
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
-      {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.31", only: :dev, runtime: false},
+      {:mix_audit, "~> 2.1", only: [:dev, :test], runtime: false}
     ]
   end
 
